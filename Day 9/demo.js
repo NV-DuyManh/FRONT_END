cb3987c9079672b0cd7a15569641266996406b41
DuyManh2k5 <duymanhdnvn20092005@gmail.com>
2026-01-03 21:54:49 +0700

    JS

---
+++ Day 9/demo.js
@@ -0,0 +1,52 @@
+// var n = 1;
+
+// do {
+
+//     if (n % 2 !== 0 && n!==5) {
+//         console.log(n);
+//     }
+//     n++;
+// } while (n <= 10);
+// function test() {
+//     var a = 10; console.log(a);
+// }
+// test();
+// let x;
+// {
+//   x = 100;
+// }
+// console.log(x);
+//     function test() {
+//          let a;
+//   if (true) {
+//      a = 1;
+//   }
+//   console.log(a);
+// }
+// test();
+// function test() {
+//   if (true) {
+//     var a = 1;
+//   }
+//   console.log(a);
+// }
+// test();
+// var a = 5;
+// console.log(a);
+
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
+// (function() {
+//   var a = 1;
+// })();
+// console.log(a);
+// if (0) {
+//   console.log("Yes");
+// } else {
+//   console.log("No");
+// }
+let x;
+if (x) console.log("True");
+else console.log("False");

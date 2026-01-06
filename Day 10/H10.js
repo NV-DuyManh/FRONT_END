1b227a272b3193625f7ce17a47ff33038e13999a
DuyManh2k5 <duymanhdnvn20092005@gmail.com>
2026-01-07 00:01:59 +0700

    js

---
+++ Day 10/H10.js
@@ -0,0 +1,14 @@
+// let obj = { a: 1, b: 2 };
+// console.log(obj["a"]);
+
+// let obj = { a: 1, b: 2 };
+// console.log(Object.keys(obj));
+
+// let arr = [1, 2, 3];
+// console.log(arr.pop());
+
+// let str = "Hello World";
+// console.log(str.split(" ")[1]);
+
+let arr = ["a", "b", "c"];
+console.log(arr.slice(1));

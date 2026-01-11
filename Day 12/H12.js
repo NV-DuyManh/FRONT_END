c13bdf0cc9945f98a764567f04d9306bbde37061
DuyManh2k5 <duymanhdnvn20092005@gmail.com>
2026-01-11 22:25:22 +0700

    Js

--- Day 12/H12.js
+++ Day 12/H12.js
@@ -99,3 +99,14 @@ function run(str) {
  
     console.log(new_word);
 }
+function run() {
+	var data = [25, 154, 461, 67, 32, 43]; // define an array
+	
+	var length = data.length; // get length of array datas
+	
+	console.log(data.length); // output: 6
+	
+	data.push(length); // Add new element to datas
+	
+	console.log(data.length); // output: 7
+}

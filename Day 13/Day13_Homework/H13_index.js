dc4dde91b7f09fa25b1a6b0902d776761fc4b05c
DuyManh2k5 <duymanhdnvn20092005@gmail.com>
2026-01-12 22:02:22 +0700

    JavaScript: JS DOM

---
+++ Day 13/Day13_Homework/H13_index.js
@@ -0,0 +1,4 @@
+// tao mang chua doi tuong gom { title , img , discount }
+// bestseller card-title => thay doi chu va mau sac
+// kiem Organic Veggies & Fruits Foods theo id => lam gi do tuy
+// xem lai cac cach kiem id ,class , query ,querySelectorAll
--- Day 13/Day13_Study/S13_index.js
+++ Day 13/Day13_Study/S13_index.js
@@ -1,66 +1,63 @@
-// // kiem theo id title
-// let a = document.getElementById("navbar");
-// console.log(a);
+// kiem theo id title
+let a = document.getElementById("navbar");
+console.log(a);
 
-// // kiem theo queryselectAll =>
-// let b = document.querySelectorAll(".grapes .col");
-// console.log(b);
+// kiem theo queryselectAll =>
+let b = document.querySelectorAll(".grapes .col");
+console.log(b);
 
-// // kieem tagnam h1
-// let c = document.getElementsByTagName("h1");
-// console.log(c);
+// kiem tagname h1
+let c = document.getElementsByTagName("h1");
+console.log(c);
 
-// // kiem theo className 
-// let d = document.getElementsByClassName("fa-car-side");
-// console.log(d);
+// kiem theo className 
+let d = document.getElementsByClassName("fa-car-side");
+console.log(d);
 
-// let listCol = document.querySelectorAll(".grapes .col img"); // array 
+let listCol = document.querySelectorAll(".grapes .col img"); // array 
 
-// listCol.forEach(e => {
-
-
-// let c_red = document.getElementsByTagName("h1");
-// Array.from(c_red).forEach(s=> {
-//     s.setAttribute("class","text-danger")
-// });
+let c_red = document.getElementsByTagName("h1");
+Array.from(c_red).forEach(s => {
+  s.setAttribute("class", "text-danger")
+});
 
 const listAnh = [
-    {
-        anh: "https://cdn.pixabay.com/photo/2025/12/22/10/18/icebear-10029029_1280.jpg",
-        tieude: "Tao"
-    },
-    {
-        anh: "https://cdn.pixabay.com/photo/2025/12/27/22/06/ship-10038606_1280.jpg",
-        tieude: "Cam"
-    },
-    {
-        anh: "https://cdn.pixabay.com/photo/2024/10/28/17/47/landscape-9156798_1280.jpg",
-        tieude: "Mit"
-    },
-    {
-        anh: "https://cdn.pixabay.com/photo/2025/10/07/14/20/japanese-barberry-9879260_1280.jpg",
-        tieude: "Nho"
-    },
-    {
-        anh: "https://cdn.pixabay.com/photo/2025/12/10/13/27/bird-10006302_1280.jpg",
-        tieude: "Xoai"
-    },
-    {
-        anh: "https://cdn.pixabay.com/photo/2025/12/10/08/15/autumn-district-10005775_1280.jpg",
-        tieude: "Man"
-    },
-    {
-        anh: "https://cdn.pixabay.com/photo/2025/12/18/21/01/royal-poodle-aurelio-10023295_1280.jpg",
-        tieude: "Tao"
-    },
-    {
-        anh: "https://cdn.pixabay.com/photo/2025/12/18/14/06/crocodiles-10022535_1280.jpg",
-        tieude: "Dao"
-    },
+  {
+    anh: "https://cdn.pixabay.com/photo/2025/12/22/10/18/icebear-10029029_1280.jpg",
+    tieude: "Tao"
+  },
+  {
+    anh: "https://cdn.pixabay.com/photo/2025/12/27/22/06/ship-10038606_1280.jpg",
+    tieude: "Cam"
+  },
+  {
+    anh: "https://cdn.pixabay.com/photo/2024/10/28/17/47/landscape-9156798_1280.jpg",
+    tieude: "Mit"
+  },
+  {
+    anh: "https://cdn.pixabay.com/photo/2025/10/07/14/20/japanese-barberry-9879260_1280.jpg",
+    tieude: "Nho"
+  },
+  {
+    anh: "https://cdn.pixabay.com/photo/2025/12/10/13/27/bird-10006302_1280.jpg",
+    tieude: "Xoai"
+  },
+  {
+    anh: "https://cdn.pixabay.com/photo/2025/12/10/08/15/autumn-district-10005775_1280.jpg",
+    tieude: "Man"
+  },
+  {
+    anh: "https://cdn.pixabay.com/photo/2025/12/18/21/01/royal-poodle-aurelio-10023295_1280.jpg",
+    tieude: "Tao"
+  },
+  {
+    anh: "https://cdn.pixabay.com/photo/2025/12/18/14/06/crocodiles-10022535_1280.jpg",
+    tieude: "Dao"
+  },
 ]
 let input = document.querySelector(".grapes");
 listAnh.forEach(s => {
-    input.innerHTML += `  <div class="col">
+  input.innerHTML += `  <div class="col">
           <div class="card">
             <div class="anh position: relative">
               <img src="${s.anh}" class="card-img-top" alt="..." />
@@ -87,7 +84,4 @@ listAnh.forEach(s => {
 
 
 
-// tao mang chua doi tuong gom { title , img , discount }
-// bestseller card-title => thay doi chu va mau sac 
-// kiem Organic Veggies & Fruits Foods theo id => lam gi do tuy 
-// xem lai cac cach kiem id ,class , query ,querySelectorAll
+

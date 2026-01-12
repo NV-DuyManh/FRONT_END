a2f2c6ed1804d91a1b27fbceff11c14b9c9c5773
DuyManh2k5 <duymanhdnvn20092005@gmail.com>
2026-01-12 21:32:30 +0700

    JS

---
+++ Day 13/S13.js
@@ -0,0 +1,93 @@
+// // kiem theo id title
+// let a = document.getElementById("navbar");
+// console.log(a);
+
+// // kiem theo queryselectAll =>
+// let b = document.querySelectorAll(".grapes .col");
+// console.log(b);
+
+// // kieem tagnam h1
+// let c = document.getElementsByTagName("h1");
+// console.log(c);
+
+// // kiem theo className 
+// let d = document.getElementsByClassName("fa-car-side");
+// console.log(d);
+
+// let listCol = document.querySelectorAll(".grapes .col img"); // array 
+
+// listCol.forEach(e => {
+
+
+// let c_red = document.getElementsByTagName("h1");
+// Array.from(c_red).forEach(s=> {
+//     s.setAttribute("class","text-danger")
+// });
+
+const listAnh = [
+    {
+        anh: "https://cdn.pixabay.com/photo/2025/12/22/10/18/icebear-10029029_1280.jpg",
+        tieude: "Tao"
+    },
+    {
+        anh: "https://cdn.pixabay.com/photo/2025/12/27/22/06/ship-10038606_1280.jpg",
+        tieude: "Cam"
+    },
+    {
+        anh: "https://cdn.pixabay.com/photo/2024/10/28/17/47/landscape-9156798_1280.jpg",
+        tieude: "Mit"
+    },
+    {
+        anh: "https://cdn.pixabay.com/photo/2025/10/07/14/20/japanese-barberry-9879260_1280.jpg",
+        tieude: "Nho"
+    },
+    {
+        anh: "https://cdn.pixabay.com/photo/2025/12/10/13/27/bird-10006302_1280.jpg",
+        tieude: "Xoai"
+    },
+    {
+        anh: "https://cdn.pixabay.com/photo/2025/12/10/08/15/autumn-district-10005775_1280.jpg",
+        tieude: "Man"
+    },
+    {
+        anh: "https://cdn.pixabay.com/photo/2025/12/18/21/01/royal-poodle-aurelio-10023295_1280.jpg",
+        tieude: "Tao"
+    },
+    {
+        anh: "https://cdn.pixabay.com/photo/2025/12/18/14/06/crocodiles-10022535_1280.jpg",
+        tieude: "Dao"
+    },
+]
+let input = document.querySelector(".grapes");
+listAnh.forEach(s => {
+    input.innerHTML += `  <div class="col">
+          <div class="card">
+            <div class="anh position: relative">
+              <img src="${s.anh}" class="card-img-top" alt="..." />
+              <p style="position: absolute">${s.tieude}</p>
+            </div>
+
+            <div class="card-body">
+              <h5 class="card-title">Grapes</h5>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod te incididunt
+              </p>
+            </div>
+            <div class="d-flex justify-content-around align-items-center cart mb-3">
+              <p class="mt-2" style="font-weight: bold">$4.99 / kg</p>
+              <div class="add d-flex text-center justify-content-center align-items-center text-success gap-2">
+                <i class="fa-solid fa-lock"></i>
+                <p>Add to cart</p>
+              </div>
+            </div>
+          </div>
+        </div>`
+})
+
+
+
+// tao mang chua doi tuong gom { title , img , discount }
+// bestseller card-title => thay doi chu va mau sac 
+// kiem Organic Veggies & Fruits Foods theo id => lam gi do tuy 
+// xem lai cac cach kiem id ,class , query ,querySelectorAll
